73af8bfd4ff21dbe5c8e45bce2d8b2b5a68c009c
ahmedtoukebri <ahmed.toukebri@esprit.tn>
2022-09-01 14:42:51 +0100

    Initial commit

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': "chatbot",
+    'version': '14.0.1.0.0',
+    'category' : 'test',
+    'website': odoomates.com,
+    'author': 'odoo mates',
+    'licence': 'AGPL-3',
+    'summary': 'auto chat',
+    'description': 'make an automatic chat with the client',
+    'depends': ['base'],
+    'data': ['views/chat.xml'],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+
+
+
+
+
+}
